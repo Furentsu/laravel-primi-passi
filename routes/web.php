7bb20b4051f28cca4677164454eeb8efcd45b57f
Furentsu <eugeniofranzoso@gmail.com>
2021-10-22 15:41:08 +0200

    Added three pages and the relative routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('my_homepage');
+});
+
+Route::get('/biography', function () {
+    return view('bio');
+});
+
+Route::get('/contacts', function () {
+    return view('contacts');
 });
